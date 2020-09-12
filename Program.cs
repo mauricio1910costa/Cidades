a2a5e9952cf58b5ada39a585abd703f0858d063f
Mauricio Costa <mauriciof1910@hotmail.com>
2020-09-11 21:53:26 -0300

    Adicionado as cidades do litoral Sul

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cidades do litoral Sul!");
+            Console.WriteLine("");
+            Console.WriteLine("Santos"); 
+            Console.WriteLine("São Vicente"); 
+            Console.WriteLine("Praia Grande"); 
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Peruíbe");
+        }
+    }
+}
